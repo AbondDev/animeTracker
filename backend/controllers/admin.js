d9ea83fec4b38702a819532501d7506eb6d4df31
Alan Bond <noreply>
2020-12-22 13:30:49 -0800

    finished course on creating a mern app.

--- backend/controllers/admin.js
+++ backend/controllers/admin.js
@@ -1,10 +1,11 @@
 const Anime = require('../models/Anime');
 
 module.exports.getIndex = async (req,res) => {
-  const shows = await Anime.find({});
+  const animes = await Anime.find({});
   try{
-    console.log(shows);
-    res.status(200).render('index', {shows})
+    console.log(animes);
+    // res.status(200).render('index', {animes})
+    res.json(anime);
   } catch (error) {
     console.log(error)
   }
@@ -12,18 +13,41 @@ module.exports.getIndex = async (req,res) => {
 
 module.exports.getAnime = async (req,res) => {
   const animeId = req.params.animeId;
-  console.log(animeId)
+
   const anime = await Anime.findById(animeId)
+
   try {
     console.log(anime);
-    return res.status(200).render('anime', {anime})
+    res.status(200).render('anime', {anime})
   } catch (error) {
     console.log(error);
   }
 }
 
 module.exports.getAddAnime = (req,res) => {
-  res.status(200).render('edit-anime');
+  res.status(200).render('edit-anime',{editing: false});
+}
+
+module.exports.getEditAnime = async (req,res) => {
+  const {animeId} = req.params;
+
+  const editMode = req.query.edit;
+
+  if(!editMode) {
+    return res.redirect('/')
+  }
+
+  const anime = await Anime.findById(animeId);
+
+  try {
+    if(!animeId) {
+      return res.redirect('/');
+    }
+    console.log(anime);
+    res.status(200).render('edit-anime', {anime:anime, editing: editMode})
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 module.exports.postAnime = (req,res) => {
@@ -31,13 +55,32 @@ module.exports.postAnime = (req,res) => {
   const anime = new Anime({name: name, image: image, description: description});
   anime.save();
   console.log('anime added to the database')
-  res.status(201).redirect('/');
+  res.status(201).redirect('http://localhost:3000/');
+}
+
+module.exports.postEditAnime = (req,res) => {
+  const {name,image,description, animeId} = req.body;
+  Anime.findById(animeId)
+    .then((anime) => {
+      anime.name = name;
+      anime.image = image;
+      anime.description = description;
+
+      return anime.save();
+    })
+    .then(() => {
+      console.log('item updated');
+      res.status(201).redirect('/');
+    })
+    .catch((err) => {
+      console.log(err)
+    })
 }
 
 module.exports.postDelete = async (req,res) => {
   const animeId = req.body.animeId;
 
-  const anime = Anime.findByIdAndRemove({animeId});
+  const anime = Anime.findByIdAndRemove(animeId);
   try {
     console.log(anime);
     console.log('Item Deleted');
